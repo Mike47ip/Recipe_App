60877506c8c54de54c280e10ca86632eb0150f9d
Mike Dark <agyemankojo47@gmail.com>
2024-04-12 15:00:46 +0000

    Remove RAILS_MASTER_KEY n use secret key

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module RecipeApp
     config.load_defaults 7.1
     config.assets.enabled = true
 
+    # Set the secret key base using the SECRET_KEY_BASE environment variable
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
