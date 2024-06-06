2ea4fa02dc933c5a8596083d1ea17c547503e65a
Mike Dark <agyemankojo47@gmail.com>
2024-06-06 19:11:05 +0000

    Use node.js for Javaascript runtime

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
+
+# Install bundler if it's not already installed
+gem install bundler
+
+# Install Ruby gems
 bundle install
+
+# Install JavaScript dependencies
+yarn install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean assets
 bundle exec rails assets:clean
