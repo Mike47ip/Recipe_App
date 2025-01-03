73ed7451d78f6d26b60caf6392bff8f72035f031
Mike Dark <agyemankojo47@gmail.com>
2025-01-03 16:11:06 +0000

    Redeploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,32 +1,13 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Install NVM (Node Version Manager)
-export NVM_DIR="$HOME/.nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
-# Load NVM
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-# Install and use Node.js
-nvm install 20
-nvm use 20
-
-# Install bundler if it's not already installed
-gem install bundler
-
-# Install Ruby gems
 bundle install
-
-# Install JavaScript dependencies
-yarn install
-
-# Run database migrations
-bundle exec rails db:migrate
-
-# Precompile assets
 bundle exec rails assets:precompile
-
-# Clean assets
 bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+# bundle exec rails db:migrate
